ab396239c31134b3a6f37ba5ab68fdcafbe1ed7b
FatherDenizli <fathermountain@gmail.com>
2025-05-30 16:08:48 -0400

    Hook class, and pages clases  was upgraded, and enum class was crated

---
+++ src/test/java/enums/UserCredentials.java
@@ -0,0 +1,25 @@
+package enums;
+
+public enum UserCredentials {
+    user_mustafa("orkidecloud@gmail.com", "Aurora20"),
+    user_ayse("user2@example.com", "password2"),
+    user_maryam("user3@example.com", "password3"),
+    user_faruk("user4@example.com", "password4"),
+    user_baris("user5@example.com", "password5");
+
+    private final String email;
+    private final String password;
+
+    UserCredentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
